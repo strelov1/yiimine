7d12305435acd281e642702db486a7446bf6f958
neoClassic <belyakov.u@gmail.com>
2014-01-10 07:32:41 +0400

    Yeeki перевод

---
+++ app/messages/ru/wiki.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'Edit' => 'Редактировать',
+    'History' => 'История',
+    'Page Index' => 'Главная',
+    'Change summary' => 'Комментарий',
+    'Save' => 'Сохранить',
+    'version history' => 'история изменений',
+    'Type wiki title' => 'Название документа',
+    'Title' => 'Название'
+);
--- app/modules/wiki/views/default/diff.php
+++ app/modules/wiki/views/default/diff.php
@@ -18,11 +18,11 @@ $this->menu = array(
 
 
 <h1>
-	<?php echo CHtml::link(CHtml::encode($uid), array('view', 'uid' => $uid))?>
+    <?php echo CHtml::link(CHtml::encode($uid), array('view', 'uid' => $uid)) ?>
     &nbsp;diff for&nbsp;
-	<?php echo CHtml::link('r'.$r1->id, array('view', 'uid' => $uid, 'rev' => $r1->id))?>
-	→
-	<?php echo CHtml::link('r'.$r2->id, array('view', 'uid' => $uid, 'rev' => $r2->id))?>
+    <?php echo CHtml::link('r' . $r1->id, array('view', 'uid' => $uid, 'rev' => $r1->id)) ?>
+    →
+    <?php echo CHtml::link('r' . $r2->id, array('view', 'uid' => $uid, 'rev' => $r2->id)) ?>
 </h1>
 
-<div class="wiki-diff"><?php echo $diff?></div>
+<div class="wiki-diff"><?php echo $diff ?></div>
