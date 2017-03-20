cee4481ca1ce9f66ba5ad354fd5ea6dd77d6139c
strelov1 <strelov1@gmail.com>
2017-03-20 08:07:45 +0300

    dockerized

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=mysql;dbname=yiimine',
-    'username' => (YII_ENV == 'dev' ? 'dev' : 'yiimine'),
-    'password' => (YII_ENV == 'dev' ? '' : 'kjctFEjCFk'),
+    'username' => 'yiimine',
+    'password' => 'yiimine_pass',
     'charset' => 'utf8',
     'enableQueryCache' => true,
     'enableSchemaCache' => true,
